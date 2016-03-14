0deb46ce44b237cb41f008d2064c411a592c46d1
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-03-14 15:31:53 -0300

    add keys

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ var http = require('http'),
 
 var port = process.env.PORT || 1337;
 httpProxy.createProxyServer({target:'http://13.75.94.137:4000/'}).listen(port);
-httpProxy.createProxyServer({target:'http://13.75.94.137:4000/'}).listen(443);
+
+httpProxy.createServer({
+  ssl: {
+    key: fs.readFileSync('server.key', 'utf8'),
+    cert: fs.readFileSync('server.crt ', 'utf8')
+  },
+  target: 'http://13.75.94.137:4000/',
+  secure: true // Depends on your needs, could be false.
+}).listen(443);
