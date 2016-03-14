9b9a4fcb6b7e54bfd7e0497c9ba9f03d49f86f30
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-03-14 15:16:14 -0300

    setup server and web config

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ var http = require('http'),
 
 var port = process.env.PORT || 1337;
 httpProxy.createProxyServer({target:'http://13.75.94.137:4000/'}).listen(port);
+httpProxy.createProxyServer({target:'http://13.75.94.137:4000/'}).listen(443);
