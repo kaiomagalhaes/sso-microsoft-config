32db00cefaa96b5406a9e5075c0204d56ad5e823
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-03-14 21:34:54 -0300

    node reverse proxy

---
+++ server.js
@@ -0,0 +1,5 @@
+var http = require('http'),
+  httpProxy = require('http-proxy');
+
+  var port = process.env.PORT || 1337;
+  httpProxy.createProxyServer({target:'http://13.75.94.137:4000/'}).listen(port);
